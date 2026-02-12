b17993d0d162fb851bf98b36dc8cb6f1e4d42d57
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2026-02-12 16:24:37 +0530

    update

--- src/api/product/routes/product.js
+++ src/api/product/routes/product.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
-/**
- * product router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::product.product');
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/products",
+      handler: "custom-product.list",
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: "GET",
+      path: "/product/:slug",
+      handler: "custom-product.detail",
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: "PUT",
+      path: "/products/:productId/update-variation",
+      handler: "custom-product.updateVariation",
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
